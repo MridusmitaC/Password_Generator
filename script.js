5058ba9d0804506b064a3013e5e45b0ecf2373f7
Mridusmita Choudhury <113635741+MridusmitaC@users.noreply.github.com>
2023-06-21 06:35:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ class Password {
   constructor() {
     this.Uchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     this.Lchar = "abcdefghijklmnopqrstuvwxyz"
-    this.num = "1234567890"
+    this.num = "0123456789"
     this.sym = "!@#$%^&*()?{}[]~"
   }
   funnyPassword() {
-    let arr = ["forgot", "incorrect","HelloWorld", "password", "12345678", "iforgot", "secret", "MyPreciousssPasssword", "MyName", "Hackerman", "nothing", "user", "maiNahiBataunga", "Sorry", "1$perhour", "admin", "passwordIs1234", "KyuChahiyePassword", "NehiBataungaPassword", "WrongPassword"]
+    let arr = ["ChangeMe", "incorrect","HelloWorld", "password","Dusre_counter_pe_jaiye", "iforgot", "secret", "Whydoialwaysforget", "nothing", "KhulJaChimChim", "maiNahiBataunga", "Sorry", "passwordIs1234", "KyuChahiyePassword", "NehiBataungaPassword", "WrongPassword"]
     let i = Math.floor(Math.random() * arr.length)
     return arr[i]
   }
@@ -23,7 +23,7 @@ class Password {
   strongPassword() {
     let arr = [this.Uchar, this.Lchar, this.num]
     let pass = ""
-    for (let k = 0; k < 8; k++) {
+    for (let k = 0; k < 10; k++) {
       let i = Math.floor(Math.random() * arr.length)
       let j = Math.floor(Math.random() * arr[i].length)
       pass += arr[i][j]
@@ -33,7 +33,7 @@ class Password {
   superStrongPassword() {
     let arr = [this.Uchar, this.Lchar, this.num, this.sym]
     let pass = ""
-    for (let k = 0; k < 10; k++) {
+    for (let k = 0; k < 12; k++) {
       let i = Math.floor(Math.random() * arr.length)
       let j = Math.floor(Math.random() * arr[i].length)
       pass += arr[i][j]
@@ -65,16 +65,3 @@ btn.addEventListener("click", () => {
   disp.value = b
 })
 
-// const toastLiveExample = document.getElementById('liveToast')
-// const toast = new bootstrap.Toast(toastLiveExample)
-
-
-// copy.addEventListener("click",()=>{
-// 	if(disp.value!= ""){
-//   disp.select()
-//   disp.setSelectionRange(0, 33);
-//   document.execCommand("copy")
-//   navigator.clipboard.writeText(disp.value)
-// 	toast.show()
-// 	}
-// })
